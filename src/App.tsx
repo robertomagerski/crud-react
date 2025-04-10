aa0410c31c7e9f3e301fcb407b7bee0be066f88d
Roberto Magerski Junior <robertomagerski7@gmail.com>
2025-04-09 22:24:50 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+
+//Pages
+import Home from './pages/Home';
 
 function App() {
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom"
+import { Button } from "@/components/ui/button"
+
+// const url = "http://localhost:3000/products"
 
 const Home = () => {
   return (
     <div>
-        <header>
-            <h1>Lista de Produtos</h1>
-        </header>
+        <h1>Lista de Produtos</h1>
+        <div>
+            <Button> clique</Button>
+        </div>
         <Link to={"/lista"}>Clique aqui</Link>
     </div>
   )
